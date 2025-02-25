b4bd778e63c8139dbe43269e660b7e99e0297d20
Sumit021995 <kaku.saurav@gmail.com>
2025-02-25 21:58:29 +0530

    new commit

--- salesforce.v1/src/main/java/genericUtility/PropertiesUtility.java
+++ salesforce.v1/src/main/java/genericUtility/PropertiesUtility.java
@@ -1,5 +1,17 @@
 package genericUtility;
 
-public class PropertiesUtility {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
+public class PropertiesUtility {
+	public String fetchValueFromPropertiesFile(String key) throws FileNotFoundException , IOException
+	{
+		FileInputStream file = new FileInputStream(IPathUtility.propertyFilePath);
+		Properties prop = new Properties();
+		prop.load(file);
+		String value = prop.getProperty(key);
+		return value;
+	}
 }
