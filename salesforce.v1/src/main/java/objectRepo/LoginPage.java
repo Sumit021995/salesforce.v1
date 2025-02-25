b53364fc1ab37204e0b9bf031c2c0611226347dc
Sumit021995 <kaku.saurav@gmail.com>
2025-02-26 00:31:12 +0530

    new commit

--- salesforce.v1/src/main/java/objectRepo/LoginPage.java
+++ salesforce.v1/src/main/java/objectRepo/LoginPage.java
@@ -1,29 +1,25 @@
 package objectRepo;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import genericUtility.IPathUtility;
-
 public class LoginPage {
 	@FindBy(id="username") private WebElement unTextField;
-	@FindBy(id="inputPassword") private WebElement pwdTextField;
-	
+
 	public WebElement getUnTextField() {
 		return unTextField;
-	}
-	public WebElement getPwdTextField() {
-		return pwdTextField;
 	}
 	public LoginPage(WebDriver driver)
 	{
 		PageFactory.initElements(driver, this);
 	}
-	public void loginToApp(WebDriver driver)
+	public void loginToApp(String username ,String password)
 	{
-		getUnTextField().sendKeys();
+		getUnTextField().sendKeys(username,Keys.TAB,password,Keys.ENTER);
 	}
+	
 	
 }
