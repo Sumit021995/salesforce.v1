f448859066c8bca5eab233127eee0fdf29356824
Sumit021995 <kaku.saurav@gmail.com>
2025-02-27 21:17:03 +0530

    new commit

---
+++ salesforce.v1/src/test/java/sampleTestExecution/CreateCampainTest.java
@@ -0,0 +1,15 @@
+package sampleTestExecution;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CreateCampainTest {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		
+	}
+}
