f4a2ea59f829c7d04e51d518fd8e85f806314cf7
Sumit021995 <kaku.saurav@gmail.com>
2025-02-28 20:48:31 +0530

    new commit

--- salesforce.v1/src/main/java/genericUtility/ExcelUtility.java
+++ salesforce.v1/src/main/java/genericUtility/ExcelUtility.java
@@ -1,8 +1,21 @@
 package genericUtility;
 
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 public class ExcelUtility {
-	public List<String> fetchMultipleDataFromExcelFile(String sheetName,int row,int cell)
+	public List<String> fetchMultipleDataFromExcelFile(String sheetName,int row,int cell) throws Exception
 	{
+		List<String> excelDataList = new ArrayList<String>();
+		FileInputStream file = new FileInputStream(IPathUtility.excelFilePath);
+		Workbook wb = WorkbookFactory.create(file);
+		for()
+		
+		return excelDataList;
 		
 	}
 	
