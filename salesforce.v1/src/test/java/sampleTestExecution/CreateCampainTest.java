61ccf70fb2c1a9718706004eadfc9e3e563f001c
Sumit021995 <kaku.saurav@gmail.com>
2025-02-28 20:05:02 +0530

    new commit

--- salesforce.v1/src/test/java/sampleTestExecution/CreateCampainTest.java
+++ salesforce.v1/src/test/java/sampleTestExecution/CreateCampainTest.java
@@ -2,6 +2,7 @@ package sampleTestExecution;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Calendar;
 import java.util.Date;
 
 import org.openqa.selenium.By;
@@ -18,17 +19,21 @@ public class CreateCampainTest {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		driver.get("http://49.249.28.218:8098/");
 		driver.findElement(By.id("username")).sendKeys("rmgyantra",Keys.TAB,"rmgy@9999",Keys.ENTER);
-		SimpleDateFormat sdf = new SimpleDateFormat("hh_min_sec");
-//		String newValue = sdf.format(new Date());
+		SimpleDateFormat sdf = new SimpleDateFormat("hh_mm_ss");
+		String uniqueStringValue = sdf.format(new Date());
+		SimpleDateFormat dateSdf = new SimpleDateFormat("dd-MM-YYYY");
+		String currentDate = dateSdf.format(Calendar.getInstance().getTime());
 		
-		String campaignName= "SumitCampaign1"+sdf.format(new Date());
+//
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//span[text()='Create Campaign']")).click();
-		driver.findElement(By.name("campaignName")).sendKeys(campaignName,Keys.TAB,Keys.TAB,"5");
+		driver.findElement(By.name("campaignName")).sendKeys("SumitCampaign"+uniqueStringValue,Keys.TAB,Keys.TAB,"5");
+		driver.findElement(By.name("expectedCloseDate")).sendKeys(currentDate);
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("//button[text()='Create Campaign']")).click();
+		driver.findElement(By.name("targetAudience")).sendKeys("Target"+uniqueStringValue,Keys.TAB,"Description",Keys.TAB,Keys.ENTER);
+//		driver.findElement(By.xpath("//button[text()='Create Campaign']")).click();
 		Thread.sleep(2000);
-		String expectedAlertText = "Campaign "+campaignName+" Successfully Added";
+		String expectedAlertText = "Campaign SumitCampaign"+uniqueStringValue+" Successfully Added";
 		WebElement alertEle = driver.findElement(By.xpath("//div[@role='alert']"));
 		if(alertEle.getText().equals(expectedAlertText))
 			System.out.println("Validated true");
