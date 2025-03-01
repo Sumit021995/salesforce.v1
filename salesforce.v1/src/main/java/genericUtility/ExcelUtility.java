307a0d49a62bc3694f0a5da6788a48c42e7f0861
Sumit021995 <kaku.saurav@gmail.com>
2025-03-01 19:10:17 +0530

    new commit

--- salesforce.v1/src/main/java/genericUtility/ExcelUtility.java
+++ salesforce.v1/src/main/java/genericUtility/ExcelUtility.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.poi.hssf.record.chart.ChartStartBlockRecord;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
@@ -12,24 +13,41 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class ExcelUtility {
-	public List<String> fetchMultipleDataFromExcelFile(String sheetName,int stratRowNum ,int startCellNum) throws Exception
+	public List<String> fetchMultipleDataFromExcelFile(String sheetName,int startRowNum ,int startCellNum) throws Exception
 	{
 		List<String> excelDataList = new ArrayList<String>();
 		FileInputStream file = new FileInputStream(IPathUtility.excelFilePath);
 		Workbook wb = WorkbookFactory.create(file);
 		Sheet sheet = wb.getSheet(sheetName);
-		Row row = sheet.getRow(stratRowNum);
-		Cell cell = row.getCell(startCellNum);
 		DataFormatter stringFormatExcelData = new DataFormatter();
-		for(int i=0;i<row.getLastCellNum();i++)
-			for(int j=0;j<)
-		String data = stringFormatExcelData.formatCellValue(cell);
-		excelDataList.add(data);
-				
-			
+		for(int i=startRowNum;i<sheet.getLastRowNum();i++)
+		{
+			Row row = sheet.getRow(i);
+			for(int j=startCellNum;j<row.getLastCellNum();j++)
+			{
+				Cell cell = row.getCell(j);
+				String data = stringFormatExcelData.formatCellValue(cell);
+				excelDataList.add(data);	
+			}
+		}
+					
+		return excelDataList;
+	}
+	public List<String> fetchMultipleDataFromExcelFileWithFixedRow(String sheetName,int stratRowNum ,int startCellNum) throws Exception
+	{
+		List<String> excelDataList = new ArrayList<String>();
+		FileInputStream file = new FileInputStream(IPathUtility.excelFilePath);
+		Workbook wb = WorkbookFactory.create(file);
+		Sheet sheet = wb.getSheet(sheetName);
+		
+		DataFormatter stringFormatExcelData = new DataFormatter();
+			for(int j=0;j<row.getLastCellNum();j++)
+			{
+				String data = stringFormatExcelData.formatCellValue(cell);
+				excelDataList.add(data);	
+			}
 		
 		return excelDataList;
-		
 	}
 	
 }
