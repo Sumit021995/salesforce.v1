ffe3577a61f4651706105f6005761738bf5d01cc
Sumit021995 <kaku.saurav@gmail.com>
2025-03-02 23:23:58 +0530

    new commit

--- salesforce.v1/src/main/java/objectRepo/HomePage.java
+++ salesforce.v1/src/main/java/objectRepo/HomePage.java
@@ -7,4 +7,10 @@ public class HomePage {
 	@FindBy(linkText="Contacts") private WebElement contactLink; 
 	@FindBy(xpath="//span[text()='Create Contact']") private WebElement createContactBtn; 
 	@FindBy(name="organizationName") private WebElement organizationTextField; 
+	@FindBy(name="department") private WebElement departmentTextField; 
+	@FindBy(name="title") private WebElement titleTextField; 
+	@FindBy(name="officePhone") private WebElement officePhoneTextField; 
+	@FindBy(name="contactName") private WebElement contactNameTextField; 
+	@FindBy(name="mobile") private WebElement mobileTextField; 
+	@FindBy(name="email") private WebElement mobileTextField; 
 }
