52a8bafbdb4affeef9bfadf9474b37af03874e9b
Sumit021995 <kaku.saurav@gmail.com>
2025-03-05 20:52:08 +0530

    new commit

--- salesforce.v1/src/main/java/genericUtility/JSONUtility.java
+++ salesforce.v1/src/main/java/genericUtility/JSONUtility.java
@@ -1,9 +1,13 @@
 package genericUtility;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class JSONUtility {
     public static void main(String[] args) throws Exception {
@@ -11,12 +15,12 @@ public class JSONUtility {
         JSONObject json = new JSONObject(content);
         System.out.println("Name: " + json.getString("name"));
     }
-    public void jsonParser()
+    public void jsonParser() throws IOException, ParseException
     {
     	JSONParser jParser = new JSONParser();
     	FileReader file = new FileReader("Absolute path");
     	Object obj = jParser.parse(file);
     	//convet to JSON Object
-    	JSONObject jObj = new JSONObject()
+    	JSONObject jObj = new JSONObject(obj);
     }
 }
