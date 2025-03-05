7f4ebef645425856e6a7e65f53cd66c24430e751
Sumit021995 <kaku.saurav@gmail.com>
2025-03-05 20:42:03 +0530

    new commit

--- salesforce.v1/src/main/java/genericUtility/JSONUtility.java
+++ salesforce.v1/src/main/java/genericUtility/JSONUtility.java
@@ -11,4 +11,12 @@ public class JSONUtility {
         JSONObject json = new JSONObject(content);
         System.out.println("Name: " + json.getString("name"));
     }
+    public void jsonParser()
+    {
+    	JSONParser jParser = new JSONParser();
+    	FileReader file = new FileReader("Absolute path");
+    	Object obj = jParser.parse(file);
+    	//convet to JSON Object
+    	JSONObject jObj = new JSONObject()
+    }
 }
