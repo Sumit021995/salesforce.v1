69d0ef6d000d24de3d83462d0fa8078cc5aeff44
Sumit021995 <kaku.saurav@gmail.com>
2025-03-06 19:38:48 +0530

    new commit

--- salesforce.v1/src/main/java/genericUtility/IPathUtility.java
+++ salesforce.v1/src/main/java/genericUtility/IPathUtility.java
@@ -3,4 +3,5 @@ package genericUtility;
 public interface IPathUtility {
 	final  String propertyFilePath = ".\\src\\test\\resources\\TestData\\TestDataSalesForce.properties";
 	final String excelFilePath = ".\\src\\test\\resources\\TestData\\HrEmailed.xlsx";
+	final String jsonFilePath = ".\\src\\test\\resources\\TestData\\preCondition.json";
 }
