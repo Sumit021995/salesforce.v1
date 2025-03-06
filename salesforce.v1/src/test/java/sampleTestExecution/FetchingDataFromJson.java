1930b1ce6f1bc95c3476e0d22b337ebc2d9a4905
Sumit021995 <kaku.saurav@gmail.com>
2025-03-06 14:55:10 +0530

    new commit

--- salesforce.v1/src/test/java/sampleTestExecution/FetchingDataFromJson.java
+++ salesforce.v1/src/test/java/sampleTestExecution/FetchingDataFromJson.java
@@ -1,7 +1,19 @@
 package sampleTestExecution;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.json.JSONObject;
+
 public class FetchingDataFromJson {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception {
+		FetchingDataFromJson data = new FetchingDataFromJson();
+		System.out.println(data.jsonReader("browser"));
+	}
+	public String jsonReader(String key) throws Exception
+	{
+		String content = new String(Files.readAllBytes(Paths.get(".\\src\\test\\resources\\TestData\\preCondition.json")));
+		JSONObject jsonObj = new JSONObject(content);
+		return jsonObj.getString(key);
 	}
 }
