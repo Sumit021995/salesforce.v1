601347901c95c20049a30f3cb3ef199e161c42c7
Sumit021995 <kaku.saurav@gmail.com>
2025-03-08 23:15:23 +0530

    new commit

--- salesforce.v1/src/test/java/sampleTestExecution/FetchingDataFromJson.java
+++ salesforce.v1/src/test/java/sampleTestExecution/FetchingDataFromJson.java
@@ -5,15 +5,24 @@ import java.nio.file.Paths;
 
 import org.json.JSONObject;
 
+import genericUtility.IPathUtility;
+
 public class FetchingDataFromJson {
 	public static void main(String[] args) throws Exception {
 		FetchingDataFromJson data = new FetchingDataFromJson();
+		// BY jsonReader
 		System.out.println(data.jsonReader("browser"));
+		System.out.println(data.jsonReader("url"));
+		System.out.println(data.jsonReader("username"));
+		System.out.println(data.jsonReader("password"));
+		System.out.println("***************");
+		
 	}
 	public String jsonReader(String key) throws Exception
 	{
-		String content = new String(Files.readAllBytes(Paths.get(".\\src\\test\\resources\\TestData\\preCondition.json")));
+		String content = new String(Files.readAllBytes(Paths.get(IPathUtility.jsonFilePath)));
 		JSONObject jsonObj = new JSONObject(content);
 		return jsonObj.getString(key);
 	}
+	
 }
