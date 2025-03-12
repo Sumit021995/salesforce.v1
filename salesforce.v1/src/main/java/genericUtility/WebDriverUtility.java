0a63600d32010cb223e746afcaff90100284b81c
Sumit021995 <kaku.saurav@gmail.com>
2025-03-12 19:41:02 +0530

    new commit

--- salesforce.v1/src/main/java/genericUtility/WebDriverUtility.java
+++ salesforce.v1/src/main/java/genericUtility/WebDriverUtility.java
@@ -1,6 +1,7 @@
 package genericUtility;
 
 import java.time.Duration;
+import java.util.Set;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,16 +9,45 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebDriverUtility {
+	/**
+	 * This is a generic method to 
+	 * @param driver
+	 * @param element
+	 * @param second
+	 */
 	public void waitForElementToBeVisible(WebDriver driver,WebElement element,long second)
 	{
 		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(second));
 		wait.until(ExpectedConditions.invisibilityOf(element));
 	
 	}
+	/**
+	 * 
+	 * @param driver
+	 * @param element
+	 * @param second
+	 */
 	public void waitForElementToBeClicable(WebDriver driver,WebElement element,long second)
 	{
 		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(second));
 		wait.until(ExpectedConditions.elementToBeClickable(element));
+		
+	}
+	/**
+	 * 
+	 * @param driver
+	 * @param partialUrl
+	 */
+	public void switchToWindow(WebDriver driver,String partialUrl)
+	{
+		Set<String> allWindowIds = driver.getWindowHandles();
+		for(String window: allWindowIds)
+		{
+			driver.switchTo().window(window);
+			String actualUrl = driver.getCurrentUrl();
+			if(actualUrl.contains(partialUrl))
+				break;
+		}
 		
 	}
 }
