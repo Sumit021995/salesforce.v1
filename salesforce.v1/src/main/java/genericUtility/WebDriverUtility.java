fb92fb00fd29c6e5435c19c38e468591578b39f6
Sumit021995 <kaku.saurav@gmail.com>
2025-03-12 19:16:26 +0530

    new commit

--- salesforce.v1/src/main/java/genericUtility/WebDriverUtility.java
+++ salesforce.v1/src/main/java/genericUtility/WebDriverUtility.java
@@ -1,5 +1,15 @@
 package genericUtility;
 
-public class WebDriverUtility {
+import java.time.Duration;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WebDriverUtility {
+	public void waitForElementVisibility(WebDriver driver,WebElement element,int second)
+	{
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(second));
+		
+	}
 }
