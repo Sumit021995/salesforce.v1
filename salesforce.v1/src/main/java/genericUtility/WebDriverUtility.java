0188eabfe8524836b024f9bd552f0b521319b809
Sumit021995 <kaku.saurav@gmail.com>
2025-03-12 19:27:16 +0530

    new commit

--- salesforce.v1/src/main/java/genericUtility/WebDriverUtility.java
+++ salesforce.v1/src/main/java/genericUtility/WebDriverUtility.java
@@ -4,12 +4,14 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebDriverUtility {
 	public void waitForElementVisibility(WebDriver driver,WebElement element,int second)
 	{
 		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(second));
+		wait.until(ExpectedConditions.invisibilityOf(element));
 		
 	}
 }
