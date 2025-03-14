43fc7179132c207d34886962c32eaf6dec028712
Sumit021995 <kaku.saurav@gmail.com>
2025-03-14 20:03:33 +0530

    new commit

--- salesforce.v1/src/main/java/objectRepo/DashboardPage.java
+++ salesforce.v1/src/main/java/objectRepo/DashboardPage.java
@@ -1,7 +1,9 @@
 package objectRepo;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class DashboardPage {
 	@FindBy(linkText="Campaigns") private WebElement campaignsLink;
@@ -11,4 +13,30 @@ public class DashboardPage {
 	@FindBy(linkText="Products") private WebElement productsLink;
 	@FindBy(xpath="//*[name()='svg']") private WebElement profileIcon;
 	@FindBy(xpath="//div[text()='Logout ']") private WebElement logoutIcon;
+	
+	public WebElement getCampaignsLink() {
+		return campaignsLink;
+	}
+	public WebElement getContactLink() {
+		return contactLink;
+	}
+	public WebElement getLeadsLink() {
+		return leadsLink;
+	}
+	public WebElement getOpportunitiesLink() {
+		return opportunitiesLink;
+	}
+	public WebElement getProductsLink() {
+		return productsLink;
+	}
+	public WebElement getProfileIcon() {
+		return profileIcon;
+	}
+	public WebElement getLogoutIcon() {
+		return logoutIcon;
+	}
+	public DashboardPage(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
 }
