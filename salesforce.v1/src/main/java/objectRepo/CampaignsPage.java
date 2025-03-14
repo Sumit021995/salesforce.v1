725a04fd69cace5c0b7e91f385407b42c05d7e8b
Sumit021995 <kaku.saurav@gmail.com>
2025-03-14 20:53:56 +0530

    new commit

--- salesforce.v1/src/main/java/objectRepo/CampaignsPage.java
+++ salesforce.v1/src/main/java/objectRepo/CampaignsPage.java
@@ -1,5 +1,12 @@
 package objectRepo;
 
-public class CampaignsPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class CampaignsPage {
+	@FindBy(id="username") private WebElement unTextField;
+	@FindBy(id="inputPassword") private WebElement pwdTextField;
+	@FindBy(xpath="//select[@class='form-control']") private WebElement searchDropDown;
+	@FindBy(xpath="//input[@class='form-control']") private WebElement searchTextField;
+	@FindBy(xpath="//span[text()='Create Campaign']") private WebElement createCampaignBtn;
 }
