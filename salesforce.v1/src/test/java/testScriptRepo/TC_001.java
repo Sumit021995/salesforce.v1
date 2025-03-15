4515cd2a09fe3ac3aad523f454be4b1b4616d698
Sumit021995 <kaku.saurav@gmail.com>
2025-03-15 20:13:32 +0530

    new commit

--- salesforce.v1/src/test/java/testScriptRepo/TC_001.java
+++ salesforce.v1/src/test/java/testScriptRepo/TC_001.java
@@ -1,7 +1,24 @@
 package testScriptRepo;
 
+import org.testng.annotations.Test;
+
 import genericUtility.BaseClass;
+import genericUtility.JavaUtility;
+import objectRepo.CampaignsPage;
+import objectRepo.CreateCampaignPage;
 
 public class TC_001 extends BaseClass {
 	
+	@Test
+	public void createCampaign() throws InterruptedException
+	{
+		System.out.println("Test Exceution Started");
+		CampaignsPage cPage = new CampaignsPage(driver);
+		cPage.clickOnCreateCampaignBtn();
+		CreateCampaignPage ccPage = new CreateCampaignPage(driver);
+		JavaUtility jUtil = new JavaUtility();
+		int randomNumber = jUtil.generateRandomNumber(1000);
+		int randomNumber2 = jUtil.generateRandomNumber(100);
+		ccPage.createCampaign("Campaign0211"+randomNumber,"This is campaign Status "+randomNumber ,""+randomNumber2);
+	}
 }
