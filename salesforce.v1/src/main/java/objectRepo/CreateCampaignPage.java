928dd1e828f2f8103a1567da048e481abb949fc6
Sumit021995 <kaku.saurav@gmail.com>
2025-03-15 19:01:12 +0530

    new commit

--- salesforce.v1/src/main/java/objectRepo/CreateCampaignPage.java
+++ salesforce.v1/src/main/java/objectRepo/CreateCampaignPage.java
@@ -1,12 +1,34 @@
 package objectRepo;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class CreateCampaignPage {
+	
 	@FindBy(name="campaignName") private WebElement campaignNameTextField;
 	@FindBy(name="campaignStatus") private WebElement campaignStatusTextField;
 	@FindBy(name="targetAudience") private WebElement targetAudienceTextField;
 	@FindBy(name="description") private WebElement descriptionTextArea;
 	@FindBy(name="targetSize") private WebElement targetSizeTextField;
+	public WebElement getCampaignNameTextField() {
+		return campaignNameTextField;
+	}
+	public WebElement getCampaignStatusTextField() {
+		return campaignStatusTextField;
+	}
+	public WebElement getTargetAudienceTextField() {
+		return targetAudienceTextField;
+	}
+	public WebElement getDescriptionTextArea() {
+		return descriptionTextArea;
+	}
+	public WebElement getTargetSizeTextField() {
+		return targetSizeTextField;
+	}
+	public CreateCampaignPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+		
+	}
 }
