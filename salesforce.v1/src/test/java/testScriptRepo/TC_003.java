9a8e6ea59d51a23d058bff1193bb39ff03fecaeb
Sumit021995 <kaku.saurav@gmail.com>
2025-03-17 20:32:51 +0530

    new commit

---
+++ salesforce.v1/src/test/java/testScriptRepo/TC_003.java
@@ -0,0 +1,24 @@
+package testScriptRepo;
+
+import org.testng.annotations.Test;
+
+import genericUtility.BaseClass;
+import genericUtility.JavaUtility;
+import objectRepo.ContactPage;
+import objectRepo.CreateContactPage;
+import objectRepo.DashboardPage;
+
+public class TC_003 extends BaseClass{
+	@Test 
+	public void createContact() throws Exception
+	{
+		DashboardPage dbPage = new DashboardPage(driver);
+		dbPage.getContactLink().click();
+		new ContactPage(driver).clickOnCreateContactBtn();
+		
+		JavaUtility jUtil = new JavaUtility();
+		int randomNumber = jUtil.generateRandomNumber(10);
+		CreateContactPage ccPage = new CreateContactPage(driver);
+		ccPage.createContactWithMandatoryFields(driver,"abc", "title"+randomNumber, "Sumit", "9818425197", "selectCampaign.html", "Campaign0211", "create-contact");
+	}
+}
