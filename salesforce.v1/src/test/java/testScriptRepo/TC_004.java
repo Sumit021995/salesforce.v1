45ba7eb3c9ce3b645841ef51be7683bc53a0957a
Sumit021995 <kaku.saurav@gmail.com>
2025-03-20 20:57:29 +0530

    new commit

--- salesforce.v1/src/test/java/testScriptRepo/TC_004.java
+++ salesforce.v1/src/test/java/testScriptRepo/TC_004.java
@@ -1,22 +1,26 @@
 package testScriptRepo;
 
+import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import genericUtility.BaseClass;
+import genericUtility.WebDriverUtility;
 import objectRepo.CreateProductPage;
 import objectRepo.DashboardPage;
 import objectRepo.ProductPage;
 
 public class TC_004 extends BaseClass {
-	@Test
-	public void createProduct()
+	@Test(groups= {"Regression"})
+	public void createProduct() throws Exception
 	{
 		new DashboardPage(driver).getProductsLink().click();
 		new ProductPage(driver).getCreateProductBtn().click();
 		CreateProductPage cpPage = new CreateProductPage(driver);
 		cpPage.createProductWithMandatoryFields("Drone","Electronics",2,1500,4);
-//		new WebDriverUtility().waitForElementToBeVisible(driver,new ProductPage(driver).getCreateProductConfirmationMsg(), 5);
-//		if(new ProductPage(driver).getCreateProductConfirmationMsg().getText().equals(cpPage))
+		new WebDriverUtility().waitForElementToBeVisible(driver,new ProductPage(driver).getCreateProductConfirmationMsg(), 5);
+		Assert.assertTrue(new ProductPage(driver).getCreateProductConfirmationMsg().getText().contains("Drone"));
+		Reporter.log("Validated True",true);
 		
 	}
 }
