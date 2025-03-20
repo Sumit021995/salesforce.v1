aafc6a37add4cba718deecc1cd7aefe5cbb5262b
Sumit021995 <kaku.saurav@gmail.com>
2025-03-20 19:12:13 +0530

    new commit

--- salesforce.v1/src/test/java/testScriptRepo/TC_002.java
+++ salesforce.v1/src/test/java/testScriptRepo/TC_002.java
@@ -11,8 +11,8 @@ import objectRepo.CreateCampaignPage;
 
 public class TC_002 extends BaseClass {
 	
-	@Test
-	public void createCampaign() throws InterruptedException
+	@Test(groups= {"Smoke"})
+	public void createCampaign_002() throws Exception
 	{
 		System.out.println("Test Exceution Started");
 		CampaignsPage cPage = new CampaignsPage(driver);
@@ -21,8 +21,10 @@ public class TC_002 extends BaseClass {
 		JavaUtility jUtil = new JavaUtility();
 		int randomNumber = jUtil.generateRandomNumber(1000);
 		int randomNumber2 = jUtil.generateRandomNumber(100);
-		String calanderDetails = jUtil.getCalanderDetails("dd-MM-YYYY", randomNumber2);
+		int randomNumber3 = jUtil.generateRandomNumber(10);
+		String calanderDetails = jUtil.getCalanderDetails("dd-MM-YYYY", randomNumber3);
 		ccPage.createCampaign("Campaign0211"+randomNumber,"This is campaign Status "+randomNumber ,""+randomNumber2,calanderDetails);
+		jUtil.waitFromThread(2000);
 		Assert.assertEquals((ccPage.getCreateCampaignConfirmationMsg().getText()).contains("Campaign0211"+randomNumber), true);
 		Reporter.log("Validated True",true);
 	}
