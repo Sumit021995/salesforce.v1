e2004ba5d097f356aab750002805070c08a36851
Sumit021995 <kaku.saurav@gmail.com>
2025-03-21 09:32:29 +0530

    new commit

--- salesforce.v1/src/main/java/genericUtility/ListenersImplementation.java
+++ salesforce.v1/src/main/java/genericUtility/ListenersImplementation.java
@@ -9,57 +9,57 @@ import org.testng.Reporter;
 
 public class ListenersImplementation implements ITestListener {
 
+	String calanderDetails = new JavaUtility().getCalanderDetails("YYYYMMdd_hhmmss");
 
 	@Override
 	public void onTestStart(ITestResult result) {
 		String methodName = result.getMethod().getMethodName();
-		Reporter.log(methodName+" On Test Start Executed",true);
+		Reporter.log("✅ "+ methodName+" Test Started Successfully ",true);
 	}
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
 		String methodName = result.getMethod().getMethodName();
-		Reporter.log(methodName+" On Test Success Executed",true);
+		Reporter.log("✅ "+ methodName+" Test Executed Successfully ",true);
 	}
 
 	@Override
 	public void onTestFailure(ITestResult result) {
-		String calanderDetails = new JavaUtility().getCalanderDetails("dd_MM_YY_hh_mm_ss");
 		String methodName = result.getMethod().getMethodName();
-		Reporter.log(methodName+" On Test Failure Executed",true);
+		Reporter.log("❌ "+ methodName+" Test Executecution Failed... ",true);
+		Reporter.log("⚠️ in process to take Screenshot...",true);
 		try {
 			new SeleniumUtility().getWebPageScreenshot(BaseClass.sDriver, calanderDetails);
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 		
 	}
 
 	@Override
 	public void onTestSkipped(ITestResult result) {
 		String methodName = result.getMethod().getMethodName();
-		Reporter.log(methodName+" On Test Skipped Executed",true);
+		Reporter.log(methodName+" Test Skipped...",true);
 		
 	}
 
 	@Override
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
 		
-		
 	}
 
 	@Override
 	public void onTestFailedWithTimeout(ITestResult result) {
 		String methodName = result.getMethod().getMethodName();
-		Reporter.log(methodName+" On Test Failed With Timeout Executed",true);
+		Reporter.log("❌ "+methodName+" On Test Failed With Timeout..",true);
 		
 	}
 
 	@Override
 	public void onStart(ITestContext context) {
 		
-		Reporter.log(" On Test Start Executed",true);
+		Reporter.log("Test Start Executed",true);
 		
 	}
 
