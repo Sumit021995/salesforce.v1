e8620ef96ce082b798da3bc360b4aefc8147ebd5
Sumit021995 <kaku.saurav@gmail.com>
2025-03-21 19:39:42 +0530

    new commit

--- salesforce.v1/src/main/java/objectRepo/LeadsPage.java
+++ salesforce.v1/src/main/java/objectRepo/LeadsPage.java
@@ -1,5 +1,24 @@
 package objectRepo;
 
-public class LeadsPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class LeadsPage {
+	@FindBy(xpath="//span[text()='Create Lead']") private WebElement createLeadBtn;
+	@FindBy(xpath="//select[@class='form-control']") private WebElement searchByLeadIdDropdown;
+	@FindBy(xpath="//input[@class='form-control' and @placeholder='Search by Lead Id']") private WebElement searchByLeadIdTestField;
+	public WebElement getCreateLeadBtn() {
+		return createLeadBtn;
+	}
+	public WebElement getSearchByLeadIdDropdown() {
+		return searchByLeadIdDropdown;
+	}
+	public WebElement getSearchByLeadIdTestField() {
+		return searchByLeadIdTestField;
+	}
+	public LeadsPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
 }
