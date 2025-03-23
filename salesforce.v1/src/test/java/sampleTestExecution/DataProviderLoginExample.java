eaea204d1d7ce033265c30b7cfde3a501103154f
Sumit021995 <kaku.saurav@gmail.com>
2025-03-23 18:30:12 +0530

    new commit

--- salesforce.v1/src/test/java/sampleTestExecution/DataProviderLoginExample.java
+++ salesforce.v1/src/test/java/sampleTestExecution/DataProviderLoginExample.java
@@ -1,6 +1,7 @@
 package sampleTestExecution;
 
 import java.time.Duration;
+import java.util.HashMap;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -23,17 +24,28 @@ public class DataProviderLoginExample {
 	public void loginToShopperAndLogout(String email , String pwd ) throws InterruptedException
 	{
 		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.get("https://www.shoppersstack.com/");
-		Thread.sleep(2000);
 		driver.findElement(By.id("loginBtn")).click();
-		Thread.sleep(2000);
 		driver.findElement(By.id("Email")).sendKeys(email,Keys.TAB,pwd,Keys.ENTER);
-		Thread.sleep(2000);
 		driver.findElement(By.xpath("//span[@class='BaseBadge-root MuiBadge-root css-1rzb3uu']")).click();
-		Thread.sleep(2000);
 		driver.findElement(By.xpath("//li[text()='Logout']")).click();
-		Thread.sleep(2000);
 		driver.quit();
+	}
+	
+	@DataProvider(name="createContact")
+	public Object[][] provideCreateContactData()
+	{
+		HashMap<String, String> data1 = new HashMap<String,String>();
+		data1.put("org", null);
+		data1.put("title", null);
+		data1.put("contactName", null);
+		data1.put("mobile", null);
+		data1.put("childUrl", null);
+		Object[][] arr = {
+				{"sumitofficial021995@gmail.com","Infocus4999@"},
+				{"infocus4999@gmail.com","Infocus4999@123"}
+		};
+		return arr;
 	}
 }
