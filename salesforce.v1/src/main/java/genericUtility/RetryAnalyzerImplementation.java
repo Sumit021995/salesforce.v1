c79820abec507b75a350c864ae055743169d5b1a
Sumit021995 <kaku.saurav@gmail.com>
2025-03-29 21:47:33 +0530

    new commit

--- salesforce.v1/src/main/java/genericUtility/RetryAnalyzerImplementation.java
+++ salesforce.v1/src/main/java/genericUtility/RetryAnalyzerImplementation.java
@@ -6,7 +6,7 @@ import org.testng.Reporter;
 
 public class RetryAnalyzerImplementation implements IRetryAnalyzer {
 
-	int count=0,maxCount=3; // manual Execution
+	int count=0,maxCount=2; // manual Execution
 	@Override
 	public boolean retry(ITestResult result) {
 		Reporter.log("⚠️ Retry Analyser Started",true);
